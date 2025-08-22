0d92904fde792f568ade78f440d653df1cd7085b
jbrilha <j.brilha@campus.fct.unl.pt>
2025-08-22 01:16:45 +0100

    chore: clean up ESP32 main

--- main/esp32_main.c
+++ main/esp32_main.c
@@ -6,93 +6,39 @@
 #include "esp_netif.h"
 #include "esp_wifi.h"
 #include "esp_wifi_types_generic.h"
+#include "mem_check.h"
 #include "nvs_flash.h"
 #include "spi_lcd_touch.h"
-#include "mem_check.h"
 #include "tcp.h"
 #include "udp.h"
 
-#include "network_manager.h"
 #include "event_dispatcher.h"
-#ifdef M5STACK_CORE_BASIC
-#include "esp_lora.h"
-#endif
+#include "network_manager.h"
 
 static const char *TAG = "ESP32_MAIN";
-
-#define MAX_MSG_SIZE 128
-#define MAX_PEERS 5
-
-#define WIFI_SSID "ESP32_AP"
-#define WIFI_PASS "superSafeAP"
-
-#define UDP_SERVER_TASK_STACK_SIZE 4096
-#define UDP_SERVER_TASK_PRIORITY 5
-
-#define TCP_SERVER_TASK_STACK_SIZE 4096
-#define TCP_SERVER_TASK_PRIORITY 5
-static bool esp32_wifi_initialized = false;
-static bool esp32_scan_done = false;
-
-static void wifi_event_handler(void *arg, esp_event_base_t event_base,
-                               int32_t event_id, void *event_data) {
-    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_AP_START) {
-        ESP_LOGI(TAG, "AP started");
-    } else if (event_base == WIFI_EVENT &&
-               event_id == WIFI_EVENT_AP_STACONNECTED) {
-        ESP_LOGI(TAG, "station connected to AP");
-    } else if (event_base == WIFI_EVENT &&
-               event_id == WIFI_EVENT_AP_STADISCONNECTED) {
-        ESP_LOGI(TAG, "station disconnected from AP");
-    }
-}
-
-
-void wifi_init(void) {
-    ESP_ERROR_CHECK(esp_netif_init());
-    ESP_ERROR_CHECK(esp_event_loop_create_default());
-    esp_netif_create_default_wifi_ap();
-
-    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
-
-    esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID,
-                               &wifi_event_handler, NULL);
-
-    wifi_config_t wifi_config = {
-        .ap = {.ssid = WIFI_SSID,
-               .password = WIFI_PASS,
-               .ssid_len = strlen(WIFI_SSID),
-               .authmode = WIFI_AUTH_WPA_WPA2_PSK,
-               .max_connection = MAX_PEERS},
-    };
-
-    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_AP));
-    ESP_ERROR_CHECK(esp_wifi_set_config((wifi_interface_t)ESP_IF_WIFI_AP, &wifi_config));
-    ESP_ERROR_CHECK(esp_wifi_start());
-}
 
 void app_main(void) {
     ESP_ERROR_CHECK(nvs_flash_init());
 
-    xTaskCreate(
-        mem_check_task,   // Task function
-        "mem_check_task",       // Task name
-        4096,                   // Stack size in words
-        NULL,                   // Task parameters
-        1,                      // Priority
-        NULL                    // Task handle
+    xTaskCreate(mem_check_task,   // Task function
+                "mem_check_task", // Task name
+                4096,             // Stack size in words
+                NULL,             // Task parameters
+                1,                // Priority
+                NULL              // Task handle
     );
 
-    // event_dispatcher_init();
+    event_dispatcher_init();
 
     // wifi_init();
     //
     // vTaskDelay(pdMS_TO_TICKS(3000));
     //
-    // xTaskCreate(udp_server_task, "udp_server", UDP_SERVER_TASK_STACK_SIZE, NULL,
+    // xTaskCreate(udp_server_task, "udp_server", UDP_SERVER_TASK_STACK_SIZE,
+    // NULL,
     //             UDP_SERVER_TASK_PRIORITY, NULL);
-    // xTaskCreate(tcp_server_task, "tcp_server", TCP_SERVER_TASK_STACK_SIZE, NULL,
+    // xTaskCreate(tcp_server_task, "tcp_server", TCP_SERVER_TASK_STACK_SIZE,
+    // NULL,
     //             TCP_SERVER_TASK_PRIORITY, NULL);
 
     // xTaskCreate(
@@ -104,16 +50,11 @@ void app_main(void) {
     //     NULL                    // Task handle
     // );
 
-    xTaskCreate(
-        lcd_touch_task,   // Task function
-        "lcd_task",       // Task name
-        4096,                   // Stack size in words
-        NULL,                   // Task parameters
-        1,                      // Priority
-        NULL                    // Task handle
+    xTaskCreate(lcd_init_task,   // Task function
+                "lcd_init_task", // Task name
+                4096,            // Stack size in words
+                NULL,            // Task parameters
+                1,               // Priority
+                NULL             // Task handle
     );
-
-#ifdef M5STACK_CORE_BASIC
-    start_lora_sender_task();
-#endif
 }
