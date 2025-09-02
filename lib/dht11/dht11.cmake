fcc7974f45a9a141a2f4304015902bcb2340e0e5
jbrilha <j.brilha@campus.fct.unl.pt>
2025-09-02 22:20:29 +0100

    feat: DHT11 support on the pico

---
+++ lib/dht11/dht11.cmake
@@ -0,0 +1,13 @@
+add_library(dht11 INTERFACE)
+
+set(DHT11_SOURCES
+  ${CMAKE_CURRENT_LIST_DIR}/dht11.c
+  )
+
+target_sources(dht11 INTERFACE
+  ${DHT11_SOURCES}
+)
+
+target_include_directories(dht11 INTERFACE ${CMAKE_CURRENT_LIST_DIR})
+
+target_link_libraries(dht11 INTERFACE pico_stdlib hardware_gpio gpio_hal)
