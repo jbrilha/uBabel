4ec99f419f5f635e28b0e79ac3ee7aa6b700583f
jbrilha <j.brilha@campus.fct.unl.pt>
2025-09-07 20:02:11 +0100

    feat: add I2C HAL v1

---
+++ lib/i2c_hal/i2c_hal.cmake
@@ -0,0 +1,13 @@
+add_library(i2c_hal INTERFACE)
+
+set(I2C_HAL_SOURCES
+  ${CMAKE_CURRENT_LIST_DIR}/i2c_hal.c
+  )
+
+target_sources(i2c_hal INTERFACE
+  ${I2C_HAL_SOURCES}
+)
+
+target_include_directories(i2c_hal INTERFACE ${CMAKE_CURRENT_LIST_DIR})
+
+target_link_libraries(i2c_hal INTERFACE pico_stdlib hardware_i2c)
