f2e02bf4c45801f3b3a1b52e0f8c2a7e2a5ce092
jbrilha <j.brilha@campus.fct.unl.pt>
2025-09-10 23:25:33 +0100

    feat: add register operations to I2C HAL

--- lib/i2c_hal/i2c_hal.c
+++ lib/i2c_hal/i2c_hal.c
@@ -3,7 +3,6 @@
 #include "platform.h"
 
 #if BUILD_ESP32
-#include "driver/i2c_master.h"
 
 #define I2C_FREQ_HZ (400 * 1000)
 
@@ -15,6 +14,8 @@
 #define SCL_PIN 7
 #endif
 
+#define I2C_SCAN_TIMEOUT_MS (50)
+#define I2C_MASTER_TIMEOUT_MS (500)
 static i2c_master_bus_handle_t bus_handle = NULL;
 
 #elif BUILD_PICO
@@ -28,7 +29,7 @@ static i2c_master_bus_handle_t bus_handle = NULL;
 
 #endif
 
-const static char *TAG = "ADC_HAL";
+const static char *TAG = "I2C_HAL";
 
 static bool i2c_initialized = false;
 
@@ -58,29 +59,128 @@ bool i2c_init_on_pins(int sda, int scl) {
 
 bool i2c_init_default(void) { return i2c_init_on_pins(SDA_PIN, SCL_PIN); }
 
-bool i2c_write_register(void) {
+bool i2c_create_device(i2c_device_handle_t *device, uint16_t addr) {
 #if BUILD_ESP32
+    i2c_device_config_t dev_config = {
+        .dev_addr_length = I2C_ADDR_BIT_LEN_7,
+        .device_address = addr,
+        .scl_speed_hz = I2C_FREQ_HZ,
+    };
+
+    device->addr = addr;
+
+    esp_err_t err =
+        i2c_master_bus_add_device(bus_handle, &dev_config, &device->esp_handle);
+
+    return err == ESP_OK;
 #else
+    device->addr = addr;
+    return true; // pico doesn't do handles, so....
 #endif
-    return false;
 }
 
-bool i2c_read_register(void) {
+bool i2c_write_bytes(i2c_device_handle_t *device, uint8_t *data, size_t len) {
 #if BUILD_ESP32
+    esp_err_t err = i2c_master_transmit(device->esp_handle, data, len,
+                                        I2C_MASTER_TIMEOUT_MS);
+    return err == ESP_OK;
 #else
+    int ret = i2c_write_blocking(i2c_default, device->addr, data, len, false);
+    return ret >= 0;
 #endif
-    return false;
 }
 
-#ifdef BUILD_PICO
-bool reserved_addr(uint8_t addr) {
+bool i2c_read_bytes(i2c_device_handle_t *device, uint8_t *data, size_t len) {
+#if BUILD_ESP32
+    esp_err_t err = i2c_master_receive(device->esp_handle, data, len,
+                                       I2C_MASTER_TIMEOUT_MS);
+
+    return err == ESP_OK;
+#else
+    int ret = i2c_read_blocking(i2c_default, device->addr, data, len, false);
+
+    return ret >= 0;
+#endif
+}
+
+bool i2c_write_register(i2c_device_handle_t *device, uint8_t reg_addr,
+                        uint8_t *data, size_t len) {
+    uint8_t tx_buf[len + 1];
+    tx_buf[0] = reg_addr;
+    memcpy(&tx_buf[1], data, len);
+
+#if BUILD_ESP32
+    esp_err_t err = i2c_master_transmit(device->esp_handle, tx_buf, len + 1,
+                                        I2C_MASTER_TIMEOUT_MS);
+
+    return err == ESP_OK;
+#else
+    int ret =
+        i2c_write_blocking(i2c_default, device->addr, tx_buf, len + 1, false);
+
+    return ret >= 0;
+#endif
+}
+
+bool i2c_read_register(i2c_device_handle_t *device, uint8_t reg_addr,
+                       uint8_t *data, size_t len) {
+#if BUILD_ESP32
+    esp_err_t err = i2c_master_transmit_receive(
+        device->esp_handle, &reg_addr, 1, data, len, I2C_MASTER_TIMEOUT_MS);
+    return err == ESP_OK;
+#else
+    int ret = i2c_write_blocking(i2c_default, device->addr, &reg_addr, 1, true);
+    if (ret < 0)
+        return false;
+
+    ret = i2c_read_blocking(i2c_default, device->addr, data, len, false);
+
+    return ret >= 0;
+#endif
+}
+
+static bool reserved_addr(uint8_t addr) {
     return (addr & 0x78) == 0 || (addr & 0x78) == 0x78;
 }
 
-// from the pico-examples repo
-void i2c_scan_task(void *params) {
-    i2c_init_default();
+#if BUILD_ESP32
+void i2c_scan_loop() {
 
+    uint8_t address;
+    while (true) {
+        printf("     0  1  2  3  4  5  6  7  8  9  A  B  C  D  E  F\r\n");
+        for (int i = 0; i < 128; i += 16) {
+            printf("%02x: ", i);
+            for (int j = 0; j < 16; j++) {
+                fflush(stdout);
+                address = i + j;
+                esp_err_t ret;
+                if (reserved_addr(address)) {
+                    ret = ESP_ERR_INVALID_ARG;
+                } else {
+                    ret = i2c_master_probe(bus_handle, address,
+                                           I2C_SCAN_TIMEOUT_MS);
+                }
+
+                if (ret == ESP_OK) {
+                    printf("%02x ", address);
+                } else if (ret == ESP_ERR_TIMEOUT) {
+                    printf("UU ");
+                } else {
+                    printf("-- ");
+                }
+            }
+            printf("\r\n");
+        }
+
+        vTaskDelay(pdMS_TO_TICKS(5000));
+    }
+}
+
+#elif BUILD_PICO
+
+// from the pico-examples repo
+void i2c_scan_loop() {
     while (true) {
         printf("\nI2C Bus Scan\n");
         printf("   0  1  2  3  4  5  6  7  8  9  A  B  C  D  E  F\n");
@@ -110,7 +210,23 @@ void i2c_scan_task(void *params) {
 
         vTaskDelay(pdMS_TO_TICKS(5000));
     }
+}
+#endif
+
+void i2c_scan_task(void *params) {
+    i2c_init_default();
+
+    i2c_scan_loop();
 
     vTaskDelete(NULL);
 }
-#endif
+
+void i2c_run_scan(void) {
+    xTaskCreate(i2c_scan_task,   // Task function
+                "i2c_scan_task", // Task name
+                4096,            // Stack size in words
+                NULL,            // Task parameters
+                1,               // Priority
+                NULL             // Task handle
+    );
+}
--- lib/i2c_hal/i2c_hal.h
+++ lib/i2c_hal/i2c_hal.h
@@ -2,12 +2,33 @@
 #define I2C_HAL_H
 
 #include <stdbool.h>
+#include <stdint.h>
+#include <stddef.h>
+
+#if BUILD_ESP32
+#include "driver/i2c_master.h"
+#endif
+
+typedef struct {
+    uint16_t addr;
+#if BUILD_ESP32
+    i2c_master_dev_handle_t esp_handle;
+#endif
+} i2c_device_handle_t;
 
 bool i2c_init_default(void);
 bool i2c_init_on_pins(int sda, int scl);
 
-#ifdef BUILD_PICO
-void i2c_scan_task(void *params);
-#endif
+bool i2c_create_device(i2c_device_handle_t *device, uint16_t addr);
+
+bool i2c_write_bytes(i2c_device_handle_t *device, uint8_t *data, size_t len);
+bool i2c_read_bytes(i2c_device_handle_t *device, uint8_t *data, size_t len);
+
+bool i2c_write_register(i2c_device_handle_t *device, uint8_t reg_addr,
+                        uint8_t *data, size_t len);
+bool i2c_read_register(i2c_device_handle_t *device, uint8_t reg_addr,
+                       uint8_t *data, size_t len);
+
+void i2c_run_scan(void);
 
 #endif // !I2C_HAL_H
